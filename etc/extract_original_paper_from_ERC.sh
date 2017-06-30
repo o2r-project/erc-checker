02b93435dff0ee8d183ab306259a7d2a1b760c8e
Timmimim <timm.kuehnel@gmail.com>
2017-06-30 12:55:59 +0200

    [fix] use pwd and system /tmp directory
    Instead of navigating through directories using '../', now use `pwd` (process working directory) to locate.
    Furthermore, instead of creating barely used temporary directory in pwd, use system /tmp directory for extracted files only temporarily used.

--- etc/extract_original_paper_from_ERC.sh
+++ etc/extract_original_paper_from_ERC.sh
@@ -5,17 +5,19 @@ printf "Beginning extraction. \n\n"
 erc_ID="$1"
 path_to_image="${2%/}/data/image.tar"
 
+base_dir="$(pwd)"
+echo "$base_dir"
+
 if [ ! "$3" ] ; then
 	html_name="main.html" ;
 else
 	html_name="$3" ;
 fi
 
-temp_path="temporaryContent/$erc_ID/"
+temp_path="/tmp/imageContentERC/$erc_ID/"
 original_paper_specific_dir="original_papers/$erc_id/"
 
-mkdir temporaryContent/
-mkdir $temp_path $original_paper_specific_dir
+mkdir -p $temp_path $pwd/$original_paper_specific_dir
 
 # extract image content to a temporary storage directory including some id
 tar xvf $path_to_image -C $temp_path
@@ -28,11 +30,10 @@ cd $temp_path
 while 
 	read -r result ; 
 	do  	echo $result ; 
-		tar xf $result | cp "erc/$html_name" "../../$original_paper_specific_dir" ;
+		tar xf $result | cp "erc/$html_name" $base_dir/$original_paper_specific_dir ;
 		printf "\n" ;
 	done < <(find . -name layer.tar)
 
-cd ../../
 rm -r $temp_path/* -f
 
 
